5ae16e2d498a31d7cbad15483884ab5eb71035f7
Georgie <227040015@qq.com>
2022-01-06 09:54:49 +0000

    添加注释

--- src/Build/views/index.js
+++ src/Build/views/index.js
@@ -1,10 +1,13 @@
 import {MODEL}List from './{MODEL}List.vue'
+//可搜索的字段
 const search = [
     {SEARCH_ARR}
 ];
+//列表显示的字段
 const columns = [
     {COLUMNS_ARR}
 ];
+//要关联查询的数组
 const withArr = [
     // {key:'with_category',get:'name'}
 ];
